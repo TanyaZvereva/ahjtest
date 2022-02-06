09e15cccf25264cc4e9becb09be9296b57864c46
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-02-06 20:02:13 +0300

    new commit

---
+++ index.spec.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const { fork } = require('child_process');
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('Credit Card Validator form', () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppeteer.launch({
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test('should add do something', async () => {
+    await page.goto(baseUrl);
+  });
+});
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
