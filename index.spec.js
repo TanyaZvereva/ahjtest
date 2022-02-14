ccdeefc6deacb9a8a78e017a44fb371429c3c001
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-02-14 17:07:07 +0300

    new commit

--- index.spec.js
+++ index.spec.js
@@ -1,39 +1,28 @@
 const puppeteer = require('puppeteer');
-const { fork } = require('child_process');
+jest.setTimeout(160000)
 
-jest.setTimeout(30000); // default puppeteer timeout
-
-describe('Credit Card Validator form', () => {
+describe('cardValid', () => {
   let browser = null;
   let page = null;
-  let server = null;
-  const baseUrl = 'http://localhost:9000';
 
   beforeAll(async () => {
-    server = fork(`${__dirname}/e2e.server.js`);
-    await new Promise((resolve, reject) => {
-      server.on('error', reject);
-      server.on('message', (message) => {
-        if (message === 'ok') {
-          resolve();
-        }
-      });
-    });
-
     browser = await puppeteer.launch({
-      // headless: false, // show gui
-      // slowMo: 250,
-      // devtools: true, // show devTools
+      headless: false, // show gui
+      slowMo: 250,
+      devtools: true, // show devTools
     });
     page = await browser.newPage();
-  });
+  })
 
   afterAll(async () => {
     await browser.close();
-    server.kill();
   });
 
-  test('should add do something', async () => {
-    await page.goto(baseUrl);
-  });
-});
+  it('should be unvalid card', async () => {
+    await page.goto('file:///C:/Users/Tanya/OneDrive/Desktop/ahj%20%D0%B4%D0%BE%D0%BC%D0%B0%D1%88%D0%BD%D1%8F%D1%8F%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0/ahjtest/index.html')
+    await page.type('input', '123456')
+    await page.click('button')
+    const searchString = await page.$("div").toString()
+    await expect(searchString).resolves.toMatch('false')
+  })
+})
